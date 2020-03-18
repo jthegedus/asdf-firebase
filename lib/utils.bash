d07542f9ee8d145074116f554c4ee091762100e3
jthegedus <james@jthegedus.com>
2020-03-18 17:39:34 +1100

    feat: firebase-tools asdf plugin

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,6 +2,26 @@
 
 set -euo pipefail
 
-function helper() {
-    printf "helper functions here\\n"
+GH_REPO="https://github.com/firebase/firebase-tools"
+
+function fail() {
+    printf "🚨  %s\\n" "${1}"
+    exit 1
+}
+
+function get_os() {
+    UNAME=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+    # Then we map the output to the names used on the Github releases page
+    case "$UNAME" in
+    linux*) MACHINE=linux ;;
+    darwin*) MACHINE=macos ;;
+    esac
+
+    echo "${MACHINE}"
+}
+
+function sort_versions() {
+    sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+        LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
