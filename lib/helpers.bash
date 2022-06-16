5176df6261c1ea515d117df78d462a445885473a
James Hegedus <jthegedus@hey.com>
2022-06-17 01:34:39 +1000

    fix: sort_versions function (#23)
    * chore: bump tool versions
    
    * fix: sort_versions algorithm to sort 1,11,111,1111 correctly
    
    * chore: update pnpm lockfile and node deps
    
    * fix: ci pipeline failure on old nodejs instructions

--- lib/helpers.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-function get_github_repo() {
-	echo "https://github.com/firebase/firebase-tools"
-}
--- lib/utils.bash
+++ lib/utils.bash
@@ -67,8 +67,12 @@ function get_os_name() {
 	echo "${os_name}"
 }
 
+function get_github_repo() {
+	echo "https://github.com/firebase/firebase-tools"
+}
+
 function sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n |
+	sed 'h; s/[-]/./g; s/.p\([[:digit:]]\)/.z.\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1n -k 2,2n -k 3,3n |
 		awk '{print $2}'
 }
