5b41be64fbab220f5db686ca51a118e57ae6a552
jthegedus <jthegedus@hey.com>
2022-07-17 12:42:19 +1000

    fix: rm unused list all filter

--- lib/envs.bash
+++ lib/envs.bash
@@ -7,4 +7,3 @@ DOCS_SITE="https://firebase.google.com/docs/cli"
 TOOL_NAME="firebase"
 TOOL_TEST="firebase --help"
 MIN_SUPPORTED_VERSION="1.0.0"
-VERSIONS_TO_OMIT="" # pipe-separated list. Eg: "mytest|0.2". grep is used, so partial matches supported.
