d4196e5f5adc802dc67cec445f886c50ef7665c3
jthegedus <jthegedus@hey.com>
2022-07-17 14:35:41 +1000

    fix: restrict installable versions to filtered list all

--- lib/envs.bash
+++ lib/envs.bash
@@ -6,4 +6,5 @@ GH_REPO="https://github.com/firebase/firebase-tools"
 DOCS_SITE="https://firebase.google.com/docs/cli"
 TOOL_NAME="firebase"
 TOOL_TEST="firebase --help"
-MIN_SUPPORTED_VERSION="7.3.0" # First Firebase CLI version that shipped pre-compiled binaries.
+MIN_SUPPORTED_VERSION="7.3.0"                # First Firebase CLI version that shipped pre-compiled binaries.
+VERSIONS_TO_OMIT="1.|2.|3.|4.|5.|6.|7.1|7.2" # pipe-separated list. Eg: "mytest|0.2". grep is used, so partial matches.
